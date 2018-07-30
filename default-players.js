bc7927d9a870457b4c452cfef7b117a84040223e
yeti-detective <yeti.detective@gmail.com>
2018-07-30 19:34:32 -0400

    default data

---
+++ default-players.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    _id: 1,
+    name: 'lebron',
+    sport: 'basketball'
+  },
+  {
+    _id: 2,
+    name: 'venus',
+    sport: 'tennis'
+  },
+  {
+    _id: 3,
+    name: 'kaepernick',
+    sport: 'justice'
+  }
+];
