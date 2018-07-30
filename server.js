3f53ae20ae4e59bd4ac136dfeed2c2ab43a7a8cb
yeti-detective <yeti.detective@gmail.com>
2018-07-30 19:34:53 -0400

    added routes for posting & resetting data

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const path = require('path');
 let port = process.env.PORT || 8000;
 
 const mongo = require('mongodb').MongoClient;
+const asyncHandler = require('express-async-handler');
 
 let db;
 
 const creds = process.env.MPW || require('./mlabpw');
+const defaultPlayers = require('./default-players');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,22 +21,47 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/os-code-challenge/index.html'));
 });
 
+app.get(
+  '/athletes',
+  asyncHandler(async (req, res, next) => {
+    const athletes = await db
+      .collection('athletes')
+      .find()
+      .toArray();
+    res.send(athletes);
+  })
+);
+
 app.get('/:file', (req, res) => {
-  res.sendFile(path.join(__dirname, `dist/os-code-challenge/${req.params.file}`))
+  res.sendFile(
+    path.join(__dirname, `dist/os-code-challenge/${req.params.file}`)
+  );
 });
 
 app.post('/new/:id', (req, res) => {
-  console.log(req.body)
-  db.collection('athletes').insert({_id: req.params.id, athlete: req.body})
-    .then(res.json({ok:true}));
-})
+  db.collection('athletes')
+    .insert({ _id: req.params.id, athlete: req.body })
+    .then(res.json({ ok: true }));
+});
 
-mongo.connect(`mongodb://${creds.user}:${creds.pw}@ds159631.mlab.com:59631/opensponsorship`, (err, database) => {
-  if (err) throw err;
-  db = database;
+app.delete(
+  '/reset_data',
+  asyncHandler(async (req, res, next) => {
+    db.collection('athletes')
+      .remove({})
+      .then(db.collection('athletes').insert(defaultPlayers))
+      .then(res.json({ ok: true }));
+  })
+);
 
-  app.listen(port, () => {
-    console.log(`listening on ${port}`);
-  });
+mongo.connect(
+  `mongodb://${creds.user}:${creds.pw}@ds159631.mlab.com:59631/opensponsorship`,
+  (err, database) => {
+    if (err) throw err;
+    db = database;
 
-})
+    app.listen(port, () => {
+      console.log(`listening on ${port}`);
+    });
+  }
+);
