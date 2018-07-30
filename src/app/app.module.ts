c3b6de179083e95bbef0ef1ba31902d71423468f
yeti-detective <yeti.detective@gmail.com>
2018-07-30 17:20:16 -0400

    added athlete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AthleteComponent } from './athlete/athlete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    AthleteComponent
   ],
   imports: [
     BrowserModule,
