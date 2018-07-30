0c2450e99d60915500c278819c0a50b62b5bc861
yeti-detective <yeti.detective@gmail.com>
2018-07-30 18:47:06 -0400

    mongo connection

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 const path = require('path');
 
-const { pw } = require('./mlabpw');
-console.log(pw);
+let port = process.env.PORT || 8000;
 
-app.get()
+const mongo = require('mongodb').MongoClient;
+
+let db;
+
+const creds = process.env.MPW || require('./mlabpw');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/os-code-challenge/index.html'));
+});
+
+app.get('/:file', (req, res) => {
+  res.sendFile(path.join(__dirname, `dist/os-code-challenge/${req.params.file}`))
+});
+
+app.post('/new/:id', (req, res) => {
+  console.log(req.body)
+  db.collection('athletes').insert({_id: req.params.id, athlete: req.body})
+    .then(res.json({ok:true}));
+})
+
+mongo.connect(`mongodb://${creds.user}:${creds.pw}@ds159631.mlab.com:59631/opensponsorship`, (err, database) => {
+  if (err) throw err;
+  db = database;
+
+  app.listen(port, () => {
+    console.log(`listening on ${port}`);
+  });
+
+})
